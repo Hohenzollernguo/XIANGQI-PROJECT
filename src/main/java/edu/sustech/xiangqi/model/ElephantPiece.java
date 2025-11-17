282af9ef9ade4987e8fc5a6a3f3451a36d56fe1d
Haibarayue <2282025125@qq.com>
2025-11-17 17:40:25 +0800

    开始象

--- src/main/java/edu/sustech/xiangqi/model/ElephantPiece.java
+++ src/main/java/edu/sustech/xiangqi/model/ElephantPiece.java
@@ -1,4 +1,15 @@
 package edu.sustech.xiangqi.model;
+/**
+ * 象/相
+ */
 
-public class ElephantPiece {
+public class ElephantPiece extends AbstractPiece {
+    public ElephantPiece(String name, int row, int col, boolean isRed) {
+        super(name, row, col, isRed);
+    }
+
+    @Override
+    public boolean canMoveTo(int targetRow, int targetCol, ChessBoardModel model) {
+        return false;
+    }
 }
