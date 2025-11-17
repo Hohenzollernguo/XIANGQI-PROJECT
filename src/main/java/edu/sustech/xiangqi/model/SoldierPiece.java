b441af8b21df3b767ddab5f2598b5dccdd004b9d
Haibarayue <2282025125@qq.com>
2025-11-17 20:25:21 +0800

    士的完善

--- src/main/java/edu/sustech/xiangqi/model/SoldierPiece.java
+++ src/main/java/edu/sustech/xiangqi/model/SoldierPiece.java
@@ -17,14 +17,17 @@ public class SoldierPiece extends AbstractPiece {
         if (currentRow == targetRow && currentCol == targetCol) {
             return false;
         }
-
         int rowDiff = targetRow - currentRow;
         int colDiff = Math.abs(targetCol - currentCol);
 
         // 兵/卒的移动规则：
         // 1. 未过河前只能向前走一步
         // 2. 过河后可以向前、向左、向右走一步，但不能后退
-        if (isRed()) {
+        if (isRed()) {if (model.getPieceAt(targetRow,targetCol)!=null) {
+            if (model.getPieceAt(targetRow, targetCol).isRed()) {
+                return false;
+            }
+        }
             // 红方兵（向上走，row减小）
             boolean crossedRiver = currentRow < 5; // 过了楚河汉界
 
@@ -38,6 +41,11 @@ public class SoldierPiece extends AbstractPiece {
             }
         } else {
             // 黑方卒（向下走，row增大）
+            if (model.getPieceAt(targetRow,targetCol)!=null) {
+                if (!model.getPieceAt(targetRow, targetCol).isRed()) {
+                    return false;
+                }
+            }
             boolean crossedRiver = currentRow >= 5; // 过了楚河汉界
 
             if (!crossedRiver) {
