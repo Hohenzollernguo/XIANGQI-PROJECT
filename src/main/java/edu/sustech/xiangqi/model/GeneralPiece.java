990205055ea7f9a32cbba3b6df195d3557614b1d
Haibarayue <2282025125@qq.com>
2025-11-17 20:45:27 +0800

    除了吃子，象完成，兵完成，将还需满足不与对面的帅相见

--- src/main/java/edu/sustech/xiangqi/model/GeneralPiece.java
+++ src/main/java/edu/sustech/xiangqi/model/GeneralPiece.java
@@ -11,7 +11,37 @@ public class GeneralPiece extends AbstractPiece {
 
     @Override
     public boolean canMoveTo(int targetRow, int targetCol, ChessBoardModel model) {
-        // TODO: 实现将/帅的移动规则
+        int currentRow=getRow();
+        int currentCol=getCol();
+        if (currentRow == targetRow && currentCol == targetCol) {
+            return false;
+        }
+        int rowDiff = Math.abs(targetRow - currentRow);
+        int colDiff = Math.abs(targetCol - currentCol);
+        //王的移动规则：九宫内走一格直线；不得对面见将
+        if((rowDiff!=1&&colDiff!=1)||(rowDiff==1&&colDiff==1)){
+            return false;
+        }
+        if (isRed()){
+            if (targetRow<7||targetCol<3||targetCol>5){
+                return false;
+            }
+        }else {if (targetRow>2||targetCol<3||targetCol>5){
+            return false;
+        }
+        }
+        if (model.getPieceAt(targetRow,targetCol)!=null){
+            if (model.getPieceAt(targetRow,targetCol).isRed()){
+                return false;
+            }
+        }
+
+
+
+
+
+
+
         return true;
     }
 }
